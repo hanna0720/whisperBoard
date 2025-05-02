e5425a1eca6ebec00f67203dc636813e5de63a95
mini-project-todo-list <leehanna0720@gmail.com>
2025-05-02 18:05:55 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/whisperboard/WhisperBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.whisperboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhisperBoardApplication.class, args);
+    }
+
+}
